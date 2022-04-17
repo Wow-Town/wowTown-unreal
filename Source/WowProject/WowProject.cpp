c6dde329818a4ce66ae8b6156346db9752fbe674
DESKTOP-Q0GMEUE\larry <locking3456@gmail.com>
2022-04-17 21:12:53 +0900

    Initial commit

---
+++ Source/WowProject/WowProject.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WowProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WowProject, "WowProject" );
+
+DEFINE_LOG_CATEGORY(LogWowProject)
+ 
---
+++ Source/WowProject/WowProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WowProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWowProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWowProjectGameMode();
+};
+
+
+
